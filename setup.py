a65fd67143762811646572afbc3b35e6f835c36a
jacki.heo <38304972+Edenpartners@users.noreply.github.com>
2019-03-21 12:30:08 +0900

    Pip dependencies added

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='eden client  sdk', version='0.2', description='eden client sdk',  author='Jacki.heo', author_email='jacki.heo@edenchain.io', license='MIT', packages= find_packages() , zip_safe=False)
+setup(name='eden client  sdk', version='0.2', description='eden client sdk',  author='Jacki.heo', author_email='jacki.heo@edenchain.io', license='MIT', packages= find_packages() , 
+        install_requires=[
+            'requests',
+            'base58',
+            'cryptoconditions',
+            'python-rapidjson',
+            'pysha3',
+            'eth_account'
+        ],
+        zip_safe=False)
